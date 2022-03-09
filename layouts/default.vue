5cf708936590d571be3a73e0a96448f408500a0f
waltermitch <devmitch256@gmail.com>
2022-03-09 23:46:30 +0200

    WIP add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <TheHeader />
+
+    <div class="nuxt-container">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader'
+export default {
+  name: 'DefaultLayout',
+  components: {
+    TheHeader,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
