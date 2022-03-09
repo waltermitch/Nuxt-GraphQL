57d535aa09241e55875d40550f233c0e2dfae6ff
waltermitch <devmitch256@gmail.com>
2022-03-09 23:53:51 +0200

    add icons to use in header

---
+++ constants/constants.js
@@ -0,0 +1,26 @@
+export const HEADER_TABS = [
+  {
+    id: 1,
+    name: 'Home',
+    src: require('assets/images/icons/header/home.svg'),
+    to: '/home',
+  },
+  {
+    id: 2,
+    name: 'Review',
+    src: require('assets/images/icons/header/portfolio.svg'),
+    to: '/review',
+  },
+  {
+    id: 3,
+    name: 'Reports',
+    src: require('assets/images/icons/header/graph-line.svg'),
+    to: '/reports',
+  },
+  {
+    id: 4,
+    name: 'Admin',
+    src: require('assets/images/icons/header/person-circle.svg'),
+    to: '/admin',
+  },
+]
