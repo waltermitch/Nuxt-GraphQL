d7bebe2de1393b79d0e350f8d077619867d56002
waltermitch <devmitch256@gmail.com>
2022-03-10 22:34:52 +0200

    create reusable components to use throughout the app

---
+++ components/PageSubheader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="subheader">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageSubheader',
+}
+</script>
+
+<style lang="scss" scoped>
+.subheader {
+  display: flex;
+  justify-content: space-between;
+  padding: 16px 20px;
+  border-bottom: 1px solid $white-smoke;
+}
+</style>
