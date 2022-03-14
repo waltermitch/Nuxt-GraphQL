199310eabba8a5345380b782773cc80053bdc119
waltermitch <devmitch256@gmail.com>
2022-03-14 18:31:32 +0200

    create custom text area component for reusable textarea

---
+++ components/CustomTextarea.vue
@@ -0,0 +1,42 @@
+<template>
+  <textarea
+    name=""
+    class="textarea"
+    :style="{ width, height }"
+    @input="textareaValue($event.target.value)"
+  ></textarea>
+</template>
+
+<script>
+export default {
+  name: 'CustomTextarea',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    width: {
+      type: String,
+      default: '500px',
+    },
+    height: {
+      type: String,
+      default: '80px',
+    },
+  },
+  methods: {
+    setValue(textareaValue) {
+      this.$emit('input', textareaValue)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.textarea {
+  background: #fff;
+  border: 1px solid $gainsboro;
+  border-radius: 3px;
+  resize: none;
+}
+</style>
