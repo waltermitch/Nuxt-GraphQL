fb78465604a30f896cf98e49c99be0cc359fd097
waltermitch <devmitch256@gmail.com>
2022-03-14 16:42:42 +0200

    create CustomTable and CustomEventRow component for reusable table

---
+++ components/CustomTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="table">
+    <div class="header">
+      <slot name="header"></slot>
+    </div>
+
+    <div class="content">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomTable',
+}
+</script>
+
+<style lang="scss" scoped>
+.table {
+  width: 100%;
+  max-width: 1105px;
+  background: #fff;
+  border: 1px solid $gainsboro;
+  border-radius: 8px;
+}
+
+.header {
+  padding: 22px 20px 25px;
+  background: $cultured;
+  border-bottom: 1px solid $white-smoke;
+
+  span {
+    color: $night-rider;
+    font-size: $font-s;
+    font-weight: 600;
+    line-height: 16px;
+    letter-spacing: 5%;
+  }
+}
+</style>
---
+++ components/CustomTableRow.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomTableRow',
+}
+</script>
+
+<style lang="scss" scoped>
+.row {
+  padding: 10px 20px;
+  border-bottom: 1px solid $white-smoke;
+}
+</style>
