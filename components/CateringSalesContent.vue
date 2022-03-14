77edaeed9c936c0237d30e51ae217f9a7375cc7f
waltermitch <devmitch256@gmail.com>
2022-03-14 15:27:27 +0200

    create CatertingSalesDetails tab and CateRingSalesContent components

---
+++ components/CateringSalesContent.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="content">
+    <PageSubheader>
+      <PageSubheaderItem>
+        <template #title>398</template>
+
+        <template #subtitle>Unit Number</template>
+      </PageSubheaderItem>
+
+      <PageSubheaderItem>
+        <template #title>09/21/2019</template>
+
+        <template #subtitle>Period End</template>
+      </PageSubheaderItem>
+    </PageSubheader>
+
+    <PageContentWrapper>
+      <TabsView :tabs-headers="tabsHeaders" :tabs="tabs"></TabsView>
+    </PageContentWrapper>
+  </div>
+</template>
+
+<script>
+/* eslint-disable vue/no-unused-components */
+import PageSubheader from './PageSubheader.vue'
+import PageSubheaderItem from './PageSubheaderItem.vue'
+import TabsView from './TabsView.vue'
+import CateringSalesDetails from './CateringSalesDetails.vue'
+import PageContentWrapper from './PageContentWrapper.vue'
+export default {
+  name: 'CateringSalesContent',
+  components: {
+    PageSubheader,
+    PageSubheaderItem,
+    TabsView,
+    CateringSalesDetails,
+    PageContentWrapper,
+  },
+  data() {
+    return {
+      tabsHeaders: ['Details', 'Items', 'Ship To/Bill'],
+      tabs: [CateringSalesDetails],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  div.subheader {
+    justify-content: flex-start;
+
+    div {
+      margin-right: 120px;
+    }
+  }
+}
+</style>
---
+++ components/CateringSalesDetails.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="content">
+    <InputRow>
+      <InputWithTitle>
+        <template #title>Order Number</template>
+
+        <template #input>
+          <CustomInput v-model="orderNumber" />
+        </template>
+      </InputWithTitle>
+
+      <InputWithTitle>
+        <template #title>Description</template>
+
+        <template #input>
+          <CustomInput v-model="description" />
+        </template>
+      </InputWithTitle>
+    </InputRow>
+
+    <InputRow>
+      <InputWithTitle>
+        <template #title>Order Date</template>
+
+        <template #input>
+          <CustomInput v-model="orderDate" />
+        </template>
+      </InputWithTitle>
+
+      <InputWithTitle>
+        <template #title>Phone</template>
+
+        <template #input>
+          <CustomInput v-model="phone" />
+        </template>
+      </InputWithTitle>
+    </InputRow>
+
+    <InputRow>
+      <InputWithTitle>
+        <template #title>Delivery Date/Time</template>
+
+        <template #input>
+          <CustomInput v-model="deliveryDateAndTime" />
+        </template>
+      </InputWithTitle>
+
+      <InputWithTitle>
+        <template #title>Head Count</template>
+
+        <template #input>
+          <CustomInput v-model="headCount" />
+        </template>
+      </InputWithTitle>
+    </InputRow>
+
+    <InputRow>
+      <InputWithTitle>
+        <template #title>Ordered By</template>
+
+        <template #input>
+          <CustomInput v-model="orderedBy" />
+        </template>
+      </InputWithTitle>
+
+      <InputWithTitle>
+        <template #title>Ordered For</template>
+
+        <template #input>
+          <CustomInput v-model="orderedFor" />
+        </template>
+      </InputWithTitle>
+    </InputRow>
+
+    <InputRow>
+      <InputWithTitle margin-bottom="14px">
+        <template #title>Taxable</template>
+
+        <template #input>
+          <CustomRadioButton
+            :is-active="taxable"
+            @set-is-active="setIsTaxable"
+          />
+        </template>
+      </InputWithTitle>
+
+      <InputWithTitle>
+        <template #title>Charge #</template>
+
+        <template #input>
+          <CustomInput v-model="chargeNumber" />
+        </template>
+      </InputWithTitle>
+    </InputRow>
+
+    <InputRow>
+      <InputWithTitle margin-bottom="14px">
+        <template #title>Cash Order</template>
+
+        <template #input>
+          <CustomRadioButton
+            :is-active="cashOrder"
+            @set-is-active="setIsCashOrder"
+          />
+        </template>
+      </InputWithTitle>
+    </InputRow>
+
+    <div class="buttons-area">
+      <DefaultButton button-color-gamma="red" @event="saveEvent">
+        Save
+      </DefaultButton>
+
+      <DefaultButton button-color-gamma="white" @event="cancelEvent">
+        Cancel
+      </DefaultButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import InputWithTitle from './InputWithTitle.vue'
+import CustomInput from './CustomInput.vue'
+import InputRow from './InputRow.vue'
+import CustomRadioButton from './CustomRadioButton.vue'
+import DefaultButton from './DefaultButton.vue'
+export default {
+  name: 'CateringSalesDetails',
+  components: {
+    InputWithTitle,
+    CustomInput,
+    InputRow,
+    CustomRadioButton,
+    DefaultButton,
+  },
+  data() {
+    return {
+      orderNumber: '',
+      description: '',
+      orderDate: '',
+      phone: '',
+      deliveryDateAndTime: '',
+      headCount: '',
+      orderedBy: '',
+      orderedFor: '',
+      taxable: false,
+      chargeNumber: '',
+      cashOrder: false,
+    }
+  },
+  methods: {
+    setIsTaxable() {
+      this.taxable = !this.taxable
+    },
+    setIsCashOrder() {
+      this.cashOrder = !this.cashOrder
+    },
+    saveEvent() {
+      console.log('save')
+    },
+    cancelEvent() {
+      Object.assign(this.$data, this.$options.data.apply(this))
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.buttons-area {
+  display: flex;
+  margin-top: 50px;
+
+  button:first-child {
+    margin-right: 11px;
+  }
+}
+</style>
