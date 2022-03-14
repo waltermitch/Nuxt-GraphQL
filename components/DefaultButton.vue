bdeba0dd16a369bbc0a2f83faab0b8dfd6b2a98f
waltermitch <devmitch256@gmail.com>
2022-03-14 15:26:34 +0200

    create DefaultButton component

---
+++ components/DefaultButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <button
+    class="button"
+    :class="{
+      'button--red': buttonColorGamma === 'red',
+      'button--white': buttonColorGamma === 'white',
+    }"
+    :style="{ padding }"
+    @click="buttonEvent"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'DefaultButton',
+  props: {
+    padding: {
+      type: String,
+      default: '10px 12px',
+    },
+    buttonColorGamma: {
+      type: String,
+      default: 'red',
+    },
+  },
+  methods: {
+    buttonEvent() {
+      this.$emit('event')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  border-radius: 3px;
+  font-size: $font-s;
+  line-height: 20px;
+  font-weight: 600;
+  text-transform: uppercase;
+  cursor: pointer;
+
+  &--red {
+    color: #fff;
+    background: $brown;
+  }
+
+  &--white {
+    color: #000;
+    background: #fff;
+    border: 1px solid $gainsboro;
+  }
+}
+</style>
