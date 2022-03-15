7e6457c035eb9fc5811c5e0441907905106ebbd5
waltermitch <devmitch256@gmail.com>
2022-03-15 15:45:01 +0200

    add close week page

---
+++ pages/admin/close-week.vue
@@ -0,0 +1,43 @@
+<template>
+  <BasePageLayout>
+    <template #sideBar>
+      <SideBar :nav-tabs="navTabs" />
+    </template>
+
+    <template #header>
+      <PageSubtitle> Admin / Close Week </PageSubtitle>
+
+      <PageTitle> Close Week </PageTitle>
+    </template>
+
+    <template #content>
+      <CloseWeekContent />
+    </template>
+  </BasePageLayout>
+</template>
+
+<script>
+import { ADMIN_NAV_TABS } from '~/constants/constants'
+import BasePageLayout from '~/components/BasePageLayout.vue'
+import SideBar from '~/components/SideBar.vue'
+import PageSubtitle from '~/components/PageSubtitle.vue'
+import PageTitle from '~/components/PageTitle.vue'
+import CloseWeekContent from '~/components/CloseWeekContent.vue'
+export default {
+  name: 'CloseWeekPage',
+  components: {
+    BasePageLayout,
+    SideBar,
+    PageSubtitle,
+    PageTitle,
+    CloseWeekContent,
+  },
+  data() {
+    return {
+      navTabs: ADMIN_NAV_TABS,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
