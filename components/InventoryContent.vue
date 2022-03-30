332219a9a59773c65ad276c3436f1c4f957f5a75
waltermitch <devmitch256@gmail.com>
2022-03-30 23:30:39 +0300

    add inventory page

---
+++ components/InventoryContent.vue
@@ -0,0 +1,218 @@
+<template>
+  <div class="content">
+    <PageSubheader>
+      <PageSubheaderItem>
+        <template #title>398</template>
+
+        <template #subtitle>Unit Number</template>
+      </PageSubheaderItem>
+
+      <PageSubheaderItem>
+        <template #title>09/21/2019</template>
+
+        <template #subtitle>Period End</template>
+      </PageSubheaderItem>
+    </PageSubheader>
+
+    <PageContentWrapper>
+      <CustomTable>
+        <template #header>
+          <div class="table-row">
+            <span>Category</span>
+
+            <span>Name</span>
+
+            <span>Previous Amount</span>
+
+            <span>Current Amount</span>
+          </div>
+        </template>
+
+        <template #content>
+          <ValidationObserver ref="form">
+            <CustomTableRow
+              v-for="item in items"
+              :key="item.id"
+              class="table-row"
+            >
+              <span class="table-text">
+                {{ item.category }}
+              </span>
+
+              <span class="table-text">
+                {{ item.name }}
+              </span>
+
+              <span class="table-text"> ${{ item.previousAmount }} </span>
+
+              <CustomInput
+                v-model="item.currentAmount"
+                rules="required|currency"
+                placeholder="$0.00"
+              />
+            </CustomTableRow>
+
+            <CustomTableRow class="table-row footer">
+              <span class="table-text"> Total: </span>
+
+              <span class="table-text">${{ totalPreviousAmount }}</span>
+
+              <span class="table-text">${{ totalCurrentValue }}</span>
+            </CustomTableRow>
+          </ValidationObserver>
+        </template>
+      </CustomTable>
+
+      <div class="buttons-area">
+        <DefaultButton button-color-gamma="red" @event="saveEvent">
+          Save
+        </DefaultButton>
+
+        <DefaultButton button-color-gamma="white" @event="cancelEvent"
+          >Cancel</DefaultButton
+        >
+      </div>
+    </PageContentWrapper>
+  </div>
+</template>
+
+<script>
+import { ValidationObserver } from 'vee-validate'
+import { mapActions } from 'vuex'
+import PageSubheader from './PageSubheader.vue'
+import PageSubheaderItem from './PageSubheaderItem.vue'
+import PageContentWrapper from './PageContentWrapper.vue'
+import CustomTable from './CustomTable.vue'
+import CustomTableRow from './CustomTableRow.vue'
+export default {
+  name: 'InventoryContent',
+  components: {
+    PageSubheader,
+    PageSubheaderItem,
+    PageContentWrapper,
+    CustomTable,
+    CustomTableRow,
+    ValidationObserver,
+  },
+  data() {
+    return {
+      totalPreviousAmount: '75.00',
+      items: [
+        {
+          id: 0,
+          category: 'Lorem ipsum',
+          name: 'Lorem ipsum',
+          previousAmount: '15.00',
+          currentAmount: '',
+        },
+        {
+          id: 1,
+          category: 'Lorem ipsum',
+          name: 'Lorem ipsum',
+          previousAmount: '25.00',
+          currentAmount: '',
+        },
+        {
+          id: 2,
+          category: 'Lorem ipsum',
+          name: 'Lorem ipsum',
+          previousAmount: '35.00',
+          currentAmount: '',
+        },
+      ],
+    }
+  },
+  computed: {
+    totalCurrentValue() {
+      return this.items.reduce((prev, current) => {
+        return Number(prev) + Number(current.currentAmount)
+      }, 0)
+    },
+  },
+  methods: {
+    ...mapActions({
+      setShowMessage: 'formSubmissionMessage/setShowMessage',
+      setMessageType: 'formSubmissionMessage/setMessageType',
+    }),
+    scrollToTop() {
+      window.scrollTo(0, 0)
+    },
+    clearState() {
+      Object.assign(this.$data, this.$options.data.apply(this))
+    },
+    saveEvent() {
+      this.$refs.form.validate().then((result) => {
+        if (result) {
+          this.setShowMessage(true)
+          this.setMessageType('success')
+          this.scrollToTop()
+          setTimeout(() => {
+            this.clearState()
+            this.$refs.form.reset()
+          }, 4000)
+        } else {
+          this.setShowMessage(true)
+          this.setMessageType('error')
+          this.scrollToTop()
+        }
+
+        setTimeout(() => {
+          this.setShowMessage(false)
+        }, 4000)
+      })
+    },
+    cancelEvent() {
+      this.$refs.form.reset()
+      this.clearState()
+      this.setShowMessage(false)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  div.subheader {
+    justify-content: flex-start;
+
+    div {
+      margin-right: 120px;
+    }
+  }
+}
+
+.table-row {
+  display: grid;
+  align-items: center;
+  grid-template-columns: 207px 346px 194px 215px;
+  column-gap: 20px;
+}
+
+.table-text {
+  font-size: $font-s;
+}
+
+.buttons-area {
+  margin-top: 40px;
+
+  button:first-child {
+    margin-right: 11px;
+  }
+}
+
+.footer {
+  height: 60px;
+
+  span:nth-child(1) {
+    grid-column: 2;
+  }
+
+  span:nth-child(2) {
+    grid-column: 3;
+  }
+
+  span:nth-child(3) {
+    grid-column: 4;
+  }
+}
+</style>
---
+++ pages/home/inventory.vue
@@ -0,0 +1,42 @@
+<template>
+  <BasePageLayout>
+    <template #sideBar>
+      <SideBar :nav-tabs="navTabs" />
+    </template>
+
+    <template #header>
+      <PageSubtitle>Home / Inventory</PageSubtitle>
+      <PageTitle> Inventory </PageTitle>
+    </template>
+
+    <template #content>
+      <InventoryContent />
+    </template>
+  </BasePageLayout>
+</template>
+
+<script>
+import { HOME_NAV_TABS } from '~/constants/constants'
+import BasePageLayout from '~/components/BasePageLayout.vue'
+import SideBar from '~/components/SideBar.vue'
+import PageTitle from '~/components/PageTitle'
+import PageSubtitle from '~/components/PageSubtitle.vue'
+import InventoryContent from '~/components/InventoryContent.vue'
+export default {
+  name: 'InventoryPage',
+  components: {
+    BasePageLayout,
+    SideBar,
+    PageTitle,
+    PageSubtitle,
+    InventoryContent,
+  },
+  data() {
+    return {
+      navTabs: HOME_NAV_TABS,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
