e2605dfedd8fef31dcdefc5bd312c7a4653e37d9
waltermitch <devmitch256@gmail.com>
2022-04-01 16:18:03 +0300

    update home page to redirect to home/select-unit

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <p>SideBar</p>
+  <p>Home page</p>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  middleware: 'redirectToSpecificPage',
+  meta: {
+    redirectTo: '/home/select-unit',
+  },
 }
 </script>
