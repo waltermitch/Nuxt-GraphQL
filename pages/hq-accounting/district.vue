42723f70e0eaa4aa5c3601d007d4864ef29b17bc
waltermitch <devmitch256@gmail.com>
2022-04-14 20:59:53 +0300

    WIP/ create page placeholders for HQ City and District

---
+++ pages/hq-accounting/district.vue
@@ -0,0 +1,41 @@
+<template>
+  <BasePageLayout>
+    <template #sideBar>
+      <SideBar :nav-tabs="navTabs" />
+    </template>
+
+    <template #header>
+      <PageSubtitle> Accounting / District </PageSubtitle>
+
+      <PageTitle> District </PageTitle>
+    </template>
+
+    <template #content>
+      <HQDistrictContent />
+    </template>
+  </BasePageLayout>
+</template>
+
+<script>
+import { HQ_ACCOUNTING } from '~/constants/constants'
+import { sideBarNavTabsMixin } from '~/mixins/sideBarNavTabsMixin'
+import BasePageLayout from '~/components/BasePageLayout.vue'
+import SideBar from '~/components/SideBar.vue'
+import PageSubtitle from '~/components/PageSubtitle.vue'
+import PageTitle from '~/components/PageTitle.vue'
+import HQDistrictContent from '~/components/HQDistrictContent.vue'
+export default {
+  name: 'DistrictPage',
+  components: {
+    BasePageLayout,
+    SideBar,
+    PageSubtitle,
+    PageTitle,
+    HQDistrictContent,
+  },
+  mixins: [sideBarNavTabsMixin(HQ_ACCOUNTING)],
+  layout: 'hqlayout',
+}
+</script>
+
+<style lang="scss" scoped></style>
