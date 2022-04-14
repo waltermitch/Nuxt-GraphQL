cd85e506d016e28b896f0aea1cf7615db5347440
waltermitch <devmitch256@gmail.com>
2022-04-14 21:25:26 +0300

    add vendor page layout

---
+++ components/HQVendorContent.vue
@@ -0,0 +1,70 @@
+<template>
+  <PageContentWrapper>
+    <CustomTable class="table">
+      <template #header>
+        <div class="table-row">
+          <span>Vendor Number</span>
+
+          <span>Vendor Name</span>
+
+          <span>Terms</span>
+        </div>
+      </template>
+
+      <template #content>
+        <CustomTableRow
+          v-for="vendor in vendors"
+          :key="vendor.id"
+          class="table-row"
+        >
+          <CustomInput v-model="vendor.vendorNumber" />
+
+          <CustomInput v-model="vendor.vendorName" />
+
+          <CustomInput v-model="vendor.terms" />
+        </CustomTableRow>
+      </template>
+    </CustomTable>
+  </PageContentWrapper>
+</template>
+
+<script>
+import PageContentWrapper from './PageContentWrapper.vue'
+import CustomTable from './CustomTable.vue'
+import CustomInput from './CustomInput.vue'
+import CustomTableRow from './CustomTableRow.vue'
+export default {
+  name: 'HQVendorContent',
+  components: {
+    PageContentWrapper,
+    CustomTable,
+    CustomInput,
+    CustomTableRow,
+  },
+  data() {
+    return {
+      vendors: [
+        {
+          id: 0,
+          vendorNumber: 'ABC01',
+          vendorName: 'ABC VENDOR',
+          terms: 'upon receipt',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.table {
+  width: fit-content;
+}
+
+.table-row {
+  display: grid;
+  align-items: center;
+  grid-template-columns: 100px repeat(2, 200px);
+  column-gap: 30px;
+}
+</style>
---
+++ pages/hq-accounting/vendor.vue
@@ -0,0 +1,41 @@
+<template>
+  <BasePageLayout>
+    <template #sideBar>
+      <SideBar :nav-tabs="navTabs" />
+    </template>
+
+    <template #header>
+      <PageSubtitle> Accounting / Vendor </PageSubtitle>
+
+      <PageTitle> Vendor </PageTitle>
+    </template>
+
+    <template #content>
+      <HQVendorContent />
+    </template>
+  </BasePageLayout>
+</template>
+
+<script>
+import { HQ_ACCOUNTING } from '~/constants/constants'
+import { sideBarNavTabsMixin } from '~/mixins/sideBarNavTabsMixin'
+import BasePageLayout from '~/components/BasePageLayout.vue'
+import SideBar from '~/components/SideBar.vue'
+import PageSubtitle from '~/components/PageSubtitle.vue'
+import PageTitle from '~/components/PageTitle.vue'
+import HQVendorContent from '~/components/HQVendorContent.vue'
+export default {
+  name: 'VendorPage',
+  components: {
+    BasePageLayout,
+    SideBar,
+    PageSubtitle,
+    PageTitle,
+    HQVendorContent,
+  },
+  mixins: [sideBarNavTabsMixin(HQ_ACCOUNTING)],
+  layout: 'hqlayout',
+}
+</script>
+
+<style lang="scss" scoped></style>
