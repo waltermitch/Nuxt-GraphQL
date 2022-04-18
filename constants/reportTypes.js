00b55453d5404c75ce3b08a057abb73d4ec6d562
waltermitch <devmitch256@gmail.com>
2022-04-18 18:55:30 +0300

    add weekly operating reports

---
+++ constants/reportTypes.js
@@ -0,0 +1,42 @@
+export const WeeklyReportsTypes = [
+  {
+    id: 0,
+    value: 'weeklyOperating',
+    name: 'Weekly Operating',
+  },
+  {
+    id: 1,
+    value: 'weeklyComplete',
+    name: 'Weekly Complete',
+  },
+  {
+    id: 2,
+    value: 'weeklyCatering',
+    name: 'Weekly Catering',
+  },
+  {
+    id: 3,
+    value: 'weeklyInventory',
+    name: 'Weekly Inventory',
+  },
+  {
+    id: 4,
+    value: 'weeklyExpense',
+    name: 'Weekly Expense',
+  },
+  {
+    id: 5,
+    value: 'weeklyLabor',
+    name: 'Weekly Labor',
+  },
+  {
+    id: 6,
+    value: 'purchaseDetails',
+    name: 'Purchase Details',
+  },
+  {
+    id: 7,
+    value: 'registerCloseouts',
+    name: 'Register Closeouts',
+  },
+]
---
+++ pages/hq-reports/weekly-operating-reports.vue
@@ -0,0 +1,41 @@
+<template>
+  <BasePageLayout>
+    <template #sideBar>
+      <SideBar :nav-tabs="navTabs" />
+    </template>
+
+    <template #header>
+      <PageSubtitle> Reports / Multi Unit Labor Report </PageSubtitle>
+
+      <PageTitle> Labor Schedule Report </PageTitle>
+    </template>
+
+    <template #content>
+      <HQWeeklyOperatingReports />
+    </template>
+  </BasePageLayout>
+</template>
+
+<script>
+import { HQ_REPORT_TABS } from '~/constants/constants'
+import { sideBarNavTabsMixin } from '~/mixins/sideBarNavTabsMixin'
+import BasePageLayout from '~/components/BasePageLayout.vue'
+import SideBar from '~/components/SideBar.vue'
+import PageSubtitle from '~/components/PageSubtitle.vue'
+import PageTitle from '~/components/PageTitle.vue'
+import HQWeeklyOperatingReports from '~/components/HQWeeklyOperatingReports.vue'
+export default {
+  name: 'WeeklyOperatingReportsPage',
+  components: {
+    BasePageLayout,
+    SideBar,
+    PageSubtitle,
+    PageTitle,
+    HQWeeklyOperatingReports,
+  },
+  mixins: [sideBarNavTabsMixin(HQ_REPORT_TABS)],
+  layout: 'hqlayout',
+}
+</script>
+
+<style lang="scss" scoped></style>
