2e59f8c589bf13103f7e6187bbf8b156565e0ab0
waltermitch <devmitch256@gmail.com>
2022-04-20 19:03:37 +0300

    add CRUD to HQContentCity

--- components/HQCityContent.vue
+++ components/HQCityContent.vue
@@ -1,46 +1,120 @@
 <template>
   <PageContentWrapper>
-    <CustomTable class="table">
-      <template #header>
-        <div class="table-row">
-          <span> State </span>
+    <ValidationObserver ref="form">
+      <CustomTable class="table">
+        <template #header>
+          <div class="table-row">
+            <span> State </span>
 
-          <span> City </span>
+            <span> City </span>
 
-          <span> City Sales Tax </span>
-        </div>
-      </template>
+            <span> City Sales Tax </span>
+          </div>
+        </template>
 
-      <template #content>
-        <CustomTableRow v-for="city in cities" :key="city.id" class="table-row">
-          <CustomSelect
-            :options="states"
-            :selected-item="states.find((state) => state.id === city.state_id)"
-            select-by="state"
-            @input="selectState($event, city.id)"
-          />
+        <template v-if="cities" #content>
+          <CustomTableRow
+            v-for="city in cities.data"
+            :key="city.id"
+            class="table-row"
+          >
+            <CustomSelect
+              v-if="isEdit === city.id"
+              :options="states.data"
+              :selected-item="city.state"
+              select-by="code"
+              @input="selectState"
+            />
+            <span v-else>
+              {{ city.state.code }}
+            </span>
 
-          <CustomInput v-model="city.name" />
+            <CustomInput
+              v-if="isEdit === city.id"
+              v-model="city.name"
+              rules="required"
+            />
+            <span v-else>
+              {{ city.name }}
+            </span>
 
-          <CustomInput v-model="city.citySalesTax" />
-        </CustomTableRow>
-      </template>
-    </CustomTable>
+            <CustomInput
+              v-if="isEdit === city.id"
+              v-model.number="city.tax"
+              rules="required|double"
+            />
+            <span v-else>
+              {{ addPercentSign(city.tax) }}
+            </span>
 
-    <DefaultButton class="button" @event="saveChanges">Save</DefaultButton>
+            <CustomTableIconsColumn
+              :is-edit-active="isEdit === city.id"
+              :is-delete-active="isDelete === city.id"
+              @edit="edit(city.id)"
+              @delete="deleteItem(city.id)"
+              @cancel="cancelEdit"
+              @cancel-delete="cancelDelete"
+              @confirm-edit="confirmEdit(city)"
+              @confirm-delete="confirmDelete(city.id)"
+            />
+          </CustomTableRow>
+
+          <CustomTableRow v-if="isAdd" class="table-row">
+            <CustomSelect
+              :options="states.data"
+              select-by="code"
+              @input="selectState"
+            />
+
+            <CustomInput v-model="cityNew.name" rules="required" />
+
+            <CustomInput
+              v-model.number="cityNew.tax"
+              type="number"
+              rules="required|double"
+            />
+          </CustomTableRow>
+
+          <CustomTableRow class="table-row add-row">
+            <img
+              src="~assets/images/icons/home/add.svg"
+              class="icon icon--add"
+              @click="addRow"
+            />
+          </CustomTableRow>
+        </template>
+      </CustomTable>
+    </ValidationObserver>
+
+    <div v-if="isAdd" class="buttons-area">
+      <DefaultButton @event="addCity"> Add City </DefaultButton>
+
+      <DefaultButton @event="cancelAdd"> Cancel </DefaultButton>
+    </div>
   </PageContentWrapper>
 </template>
 
 <script>
+import { ValidationObserver } from 'vee-validate'
+import Cities from '../graphql/queries/cities.gql'
+import States from '../graphql/queries/states.gql'
+import CreateCity from '../graphql/mutations/city/createCity.gql'
+import UpdateCity from '../graphql/mutations/city/updateCity.gql'
+import DeleteCity from '../graphql/mutations/city/deleteCity.gql'
 import PageContentWrapper from './PageContentWrapper.vue'
 import CustomTable from './CustomTable.vue'
 import CustomTableRow from './CustomTableRow.vue'
 import CustomSelect from './CustomSelect.vue'
 import CustomInput from './CustomInput.vue'
 import DefaultButton from './DefaultButton.vue'
+import { accountingMixin } from '~/mixins/accountingMixin'
+import { submitMessagesMixin } from '~/mixins/submitMessagesMixin'
+import { formMixin } from '~/mixins/formMixin'
+import { mutationMixin } from '~/mixins/mutationMixin'
 export default {
   name: 'HQCityContent',
   components: {
+    ValidationObserver,
     PageContentWrapper,
     CustomTable,
     CustomTableRow,
@@ -48,59 +122,71 @@ export default {
     CustomInput,
     DefaultButton,
   },
+  mixins: [submitMessagesMixin, formMixin, mutationMixin, accountingMixin],
+  apollo: {
+    cities: {
+      query: Cities,
+    },
+    states: {
+      query: States,
+    },
+  },
   data() {
     return {
-      saveIsActive: false,
-      states: [
-        {
-          id: 0,
-          state: 'NY',
-          salesTaxCafeteria: '4.00%',
-          salesTaxVending: '3.90%',
-          salesTaxRestaurant: '2.90%',
-          salesTaxCStore: '2.00%',
-          grossReceiptsTax: '0.00%',
-        },
-        {
-          id: 1,
-          state: 'CA',
-          salesTaxCafeteria: '4.00%',
-          salesTaxVending: '3.90%',
-          salesTaxRestaurant: '2.90%',
-          salesTaxCStore: '2.00%',
-          grossReceiptsTax: '0.00%',
-        },
-      ],
-      cities: [
-        {
-          id: 0,
-          name: 'City',
-          state_id: 0,
-          citySalesTax: '2.3%',
-        },
-        {
-          id: 1,
-          name: 'Another City',
-          state_id: 1,
-          citySalesTax: '2.1%',
-        },
-      ],
+      cityNew: {
+        name: '',
+        state: null,
+        tax: '',
+      },
     }
   },
-  watch: {
-    cities: {
-      handler() {
-        this.saveIsActive = true
-      },
-      deep: true,
-    },
-  },
   methods: {
-    saveChanges() {
-      console.log(this.cities)
+    selectState(state) {
+      this.cityNew.state = state
     },
-    selectState(item, cityID) {
-      this.cities.find((city) => city.id === cityID).state_id = item.id
+    addCity() {
+      this.mutationAction(
+        CreateCity,
+        {
+          cityInput: {
+            name: this.cityNew.name,
+            state: {
+              connect: Number(this.cityNew.state.id),
+            },
+            tax: this.cityNew.tax,
+          },
+        },
+        Cities,
+        'Add city success',
+        'Add city error'
+      )
+    },
+    confirmEdit(city) {
+      const editedCity = {
+        id: city.id,
+        name: city.name,
+        state: {
+          connect: this.cityNew.state.id,
+        },
+        tax: city.tax,
+      }
+
+      this.mutationAction(
+        UpdateCity,
+        { cityInput: editedCity },
+        Cities,
+        'Edit city success',
+        'Edit city error'
+      )
+    },
+    confirmDelete(id) {
+      this.mutationAction(
+        DeleteCity,
+        { id },
+        Cities,
+        'Delete state success',
+        'Delete state error'
+      )
     },
   },
 }
@@ -114,11 +200,20 @@ export default {
 .table-row {
   display: grid;
   align-items: center;
-  grid-template-columns: 100px 250px 150px;
+  grid-template-columns: 100px 250px 150px 60px;
   column-gap: 30px;
 }
 
-.button {
-  margin-top: 20px;
+.icon {
+  cursor: pointer;
+
+  &--add {
+    grid-column: 4;
+    justify-self: end;
+  }
+}
+
+.buttons-area {
+  margin-top: 25px;
 }
 </style>
