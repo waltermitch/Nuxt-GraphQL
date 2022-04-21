065695f5c8b03efdb04a05bd83c29c2cdf7ba1ae
waltermitch <devmitch256@gmail.com>
2022-04-21 13:30:29 +0300

    connect API TO HQVendorContent

--- components/HQVendorContent.vue
+++ components/HQVendorContent.vue
@@ -1,57 +1,181 @@
 <template>
   <PageContentWrapper>
-    <CustomTable class="table">
-      <template #header>
-        <div class="table-row">
-          <span>Vendor Number</span>
+    <ValidationObserver ref="form">
+      <CustomTable class="table">
+        <template #header>
+          <div class="table-row">
+            <span>Vendor Number</span>
 
-          <span>Vendor Name</span>
+            <span>Vendor Name</span>
 
-          <span>Terms</span>
-        </div>
-      </template>
+            <span>Terms</span>
+          </div>
+        </template>
 
-      <template #content>
-        <CustomTableRow
-          v-for="vendor in vendors"
-          :key="vendor.id"
-          class="table-row"
-        >
-          <CustomInput v-model="vendor.vendorNumber" />
+        <template v-if="vendors" #content>
+          <CustomTableRow
+            v-for="vendor in vendors.data"
+            :key="vendor.id"
+            class="table-row"
+          >
+            <CustomInput
+              v-if="isEdit === vendor.id"
+              v-model="vendor.code"
+              rules="required"
+            />
+            <span v-else>{{ vendor.code }}</span>
 
-          <CustomInput v-model="vendor.vendorName" />
+            <CustomInput
+              v-if="isEdit === vendor.id"
+              v-model="vendor.name"
+              rules="required"
+            />
+            <span v-else>{{ vendor.name }}</span>
 
-          <CustomInput v-model="vendor.terms" />
-        </CustomTableRow>
-      </template>
-    </CustomTable>
+            <CustomSelect
+              v-if="isEdit === vendor.id"
+              :options="terms.data"
+              :selected-item="vendor.term"
+              select-by="dueDays"
+              @input="selectTerm"
+            />
+            <span v-else>{{ vendor.term.dueDays }}</span>
+
+            <CustomTableIconsColumn
+              :is-edit-active="isEdit === vendor.id"
+              :is-delete-active="isDelete === vendor.id"
+              @edit="edit(vendor.id)"
+              @delete="deleteItem(vendor.id)"
+              @cancel="cancelEdit"
+              @cancel-delete="cancelDelete"
+              @confirm-edit="confirmEdit(vendor)"
+              @confirm-delete="confirmDelete(vendor.id)"
+            />
+          </CustomTableRow>
+
+          <CustomTableRow v-if="isAdd" class="table-row">
+            <CustomInput v-model="vendorNew.code" rules="required" />
+
+            <CustomInput v-model="vendorNew.name" rules="required" />
+
+            <CustomSelect
+              :options="terms.data"
+              select-by="dueDays"
+              @input="selectTerm"
+            />
+          </CustomTableRow>
+
+          <CustomTableRow class="table-row add-row">
+            <img
+              src="~assets/images/icons/home/add.svg"
+              class="icon icon--add"
+              @click="addRow"
+            />
+          </CustomTableRow>
+        </template>
+      </CustomTable>
+    </ValidationObserver>
+
+    <div v-if="isAdd" class="buttons-area">
+      <DefaultButton @event="addVendor"> Add Vendor </DefaultButton>
+
+      <DefaultButton @event="cancelAdd"> Cancel </DefaultButton>
+    </div>
   </PageContentWrapper>
 </template>
 
 <script>
+import { ValidationObserver } from 'vee-validate'
+import Vendors from '../graphql/queries/vendors.gql'
+import Terms from '../graphql/queries/terms.gql'
+import CreateVendor from '../graphql/mutations/vendor/createVendor.gql'
+import UpdateVendor from '../graphql/mutations/vendor/updateVendor.gql'
+import DeleteVendor from '../graphql/mutations/vendor/deleteVendor.gql'
 import PageContentWrapper from './PageContentWrapper.vue'
 import CustomTable from './CustomTable.vue'
 import CustomInput from './CustomInput.vue'
 import CustomTableRow from './CustomTableRow.vue'
+import CustomSelect from './CustomSelect.vue'
+import { accountingMixin } from '~/mixins/accountingMixin'
+import { submitMessagesMixin } from '~/mixins/submitMessagesMixin'
+import { formMixin } from '~/mixins/formMixin'
+import { mutationMixin } from '~/mixins/mutationMixin'
 export default {
   name: 'HQVendorContent',
   components: {
+    ValidationObserver,
     PageContentWrapper,
     CustomTable,
     CustomInput,
     CustomTableRow,
+    CustomSelect,
+  },
+  mixins: [submitMessagesMixin, formMixin, mutationMixin, accountingMixin],
+  apollo: {
+    vendors: {
+      query: Vendors,
+    },
+    terms: {
+      query: Terms,
+    },
   },
   data() {
     return {
-      vendors: [
-        {
-          id: 0,
-          vendorNumber: 'ABC01',
-          vendorName: 'ABC VENDOR',
-          terms: 'upon receipt',
-        },
-      ],
+      vendorNew: {
+        code: '',
+        name: '',
+        term: null,
+      },
     }
+  },
+  methods: {
+    selectTerm(term) {
+      this.vendorNew.term = term
+    },
+    addVendor() {
+      this.mutationAction(
+        CreateVendor,
+        {
+          vendorInput: {
+            code: this.vendorNew.code,
+            name: this.vendorNew.name,
+            term: {
+              connect: this.vendorNew.term.id,
+            },
+          },
+        },
+        Vendors,
+        'Add vendor success',
+        'Add vendor error'
+      )
+    },
+    confirmEdit(vendor) {
+      const editedVendor = {
+        id: vendor.id,
+        code: vendor.code,
+        name: vendor.name,
+        term: {
+          connect: this.vendorNew.term.id,
+        },
+      }
+
+      this.mutationAction(
+        UpdateVendor,
+        { vendorInput: editedVendor },
+        Vendors,
+        'Add vendor success',
+        'Add vendor error'
+      )
+    },
+    confirmDelete(id) {
+      this.mutationAction(
+        DeleteVendor,
+        { id },
+        Vendors,
+        'Add vendor success',
+        'Add vendor error'
+      )
+    },
   },
 }
 </script>
@@ -64,7 +188,20 @@ export default {
 .table-row {
   display: grid;
   align-items: center;
-  grid-template-columns: 100px repeat(2, 200px);
+  grid-template-columns: 100px repeat(2, 200px) 60px;
   column-gap: 30px;
+}
+
+.icon {
+  cursor: pointer;
+
+  &--add {
+    grid-column: 4;
+    justify-self: end;
+  }
+}
+
+.buttons-area {
+  margin-top: 25px;
 }
 </style>
