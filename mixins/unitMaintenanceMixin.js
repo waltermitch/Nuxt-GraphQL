52ee649b40d5bab7cb8fa6143e2f8e25f21931c5
waltermitch <devmitch256@gmail.com>
2022-04-26 13:25:22 +0300

    update unitMaintenanceMixin

--- mixins/unitMaintenanceMixin.js
+++ mixins/unitMaintenanceMixin.js
@@ -3,11 +3,14 @@ export const unitMaintenanceMixin = {
   computed: {
     ...mapGetters({
       isShowAddUnit: 'unitMaintenance/getShowAddUnit',
+      unitID: 'unitMaintenance/getUnitID',
+      unit: 'unitMaintenance/getUnit',
     }),
   },
   methods: {
     ...mapActions({
       setShowAddUnit: 'unitMaintenance/setShowAddUnit',
+      setUnitID: 'unitMaintenance/setUnitID',
     }),
     showAddUnit() {
       this.setShowAddUnit('HQUnitMaintenance')
