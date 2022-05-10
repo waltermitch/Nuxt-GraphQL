054cbc08568dd2c340e51d06a51c23017c86fa83
waltermitch <devmitch256@gmail.com>
2022-05-10 22:58:12 +0300

    add untit vendors page

---
+++ components/HQUnitVendorContent.vue
@@ -0,0 +1,222 @@
+<template>
+  <PageContentWrapper>
+    <InputRow>
+      <InputWithTitle>
+        <template #title> Unit </template>
+
+        <template #input>
+          <CustomSelect
+            v-if="units"
+            :options="units.data"
+            select-by="code"
+            @input="selectUnit"
+          />
+        </template>
+      </InputWithTitle>
+
+      <InputWithTitle>
+        <template #title> Name </template>
+
+        <template #input>
+          <CustomInput v-model="unit.name" />
+        </template>
+      </InputWithTitle>
+    </InputRow>
+
+    <CustomTable v-if="unit.vendors" class="table">
+      <template #header>
+        <div class="table-row">
+          <span> Vendor ID </span>
+
+          <span> Vendor Name </span>
+
+          <span> Vendor Term</span>
+        </div>
+      </template>
+
+      <template #content>
+        <CustomTableRow
+          v-for="vendor in unit.vendors"
+          :key="vendor.id"
+          class="table-row"
+        >
+          <span>
+            {{ vendor.id }}
+          </span>
+
+          <span>
+            {{ vendor.name }}
+          </span>
+
+          <span>
+            {{ vendor.term.name }}
+          </span>
+        </CustomTableRow>
+      </template>
+    </CustomTable>
+
+    <CustomTable v-if="vendors" class="table">
+      <template #header>
+        <div class="table-row">
+          <span> Vendor ID </span>
+
+          <span> Vendor Name </span>
+
+          <span> Vendor Term</span>
+
+          <span> Add Vendor to Unit </span>
+        </div>
+      </template>
+
+      <template #content>
+        <CustomTableRow
+          v-for="vendor in vendors.data"
+          :key="vendor.id"
+          class="table-row"
+        >
+          <span>
+            {{ vendor.id }}
+          </span>
+
+          <span>
+            {{ vendor.name }}
+          </span>
+
+          <span>
+            {{ vendor.term.name }}
+          </span>
+
+          <DefaultButton
+            @event="
+              unit.vendors.find((item) => item.id === vendor.id)
+                ? removeVendorFromUnit(vendor)
+                : addVendorToUnit(vendor)
+            "
+          >
+            {{
+              unit.vendors && unit.vendors.find((item) => item.id === vendor.id)
+                ? 'Remove Vendor From Unit'
+                : 'Add Vendor to Unit'
+            }}
+          </DefaultButton>
+        </CustomTableRow>
+      </template>
+    </CustomTable>
+  </PageContentWrapper>
+</template>
+
+<script>
+import Vendors from '../graphql/queries/vendors.gql'
+import Units from '../graphql/queries/units.gql'
+import UpdateUnit from '../graphql/mutations/unit/updateUnit.gql'
+import PageContentWrapper from './PageContentWrapper.vue'
+import DefaultButton from './DefaultButton.vue'
+import { mutationMixin } from '~/mixins/mutationMixin'
+import { tableActionsMixin } from '~/mixins/tableActionsMixin'
+export default {
+  name: 'HQUnitVendorContent',
+  components: { PageContentWrapper, DefaultButton },
+  apollo: {
+    vendors: {
+      query: Vendors,
+    },
+    units: {
+      query: Units,
+    },
+  },
+  mixins: [mutationMixin, tableActionsMixin],
+  data() {
+    return {
+      unit: '',
+    }
+  },
+  methods: {
+    selectUnit(item) {
+      this.unit = item
+    },
+    async addVendorToUnit(vendor) {
+      const unit = this.unit
+      const { state, users, __typename, ...unitInput } = this.unit
+
+      await this.mutationAction(
+        UpdateUnit,
+        {
+          unitInput: {
+            ...unitInput,
+            district: {
+              connect: Number(this.unit.district.id),
+            },
+            county: {
+              connect: Number(this.unit.county.id),
+            },
+            city: {
+              connect: Number(this.unit.city.id),
+            },
+            users: {
+              sync: users.map((user) => user.id),
+            },
+            vendors: {
+              sync: [
+                ...this.unit.vendors.map((vendor) => vendor.id),
+                vendor.id,
+              ],
+            },
+          },
+        },
+        Units,
+        'Add vendor to unit success',
+        'Add vendor to unit error'
+      )
+
+      this.unit = unit
+    },
+    async removeVendorFromUnit(vendor) {
+      const unit = this.unit
+      const { state, users, __typename, ...unitInput } = this.unit
+
+      await this.mutationAction(
+        UpdateUnit,
+        {
+          unitInput: {
+            ...unitInput,
+            district: {
+              connect: Number(this.unit.district.id),
+            },
+            county: {
+              connect: Number(this.unit.county.id),
+            },
+            city: {
+              connect: Number(this.unit.city.id),
+            },
+            users: {
+              sync: users.map((user) => user.id),
+            },
+            vendors: {
+              disconnect: [Number(vendor.id)],
+            },
+          },
+        },
+        Units,
+        'Remove vendor from unit success',
+        'Remove vendor from unit error'
+      )
+
+      this.unit = unit
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.table {
+  width: fit-content;
+  margin-bottom: 20px;
+}
+
+.table-row {
+  display: grid;
+  align-items: center;
+  grid-template-columns: 80px repeat(2, 100px) 200px;
+  column-gap: 20px;
+}
+</style>
---
+++ pages/hq-unit-setup/unit-vendor.vue
@@ -0,0 +1,42 @@
+<template>
+  <BasePageLayout>
+    <template #sideBar>
+      <SideBar :nav-tabs="navTabs" />
+    </template>
+
+    <template #header>
+      <PageSubtitle> Unit Setup / Unit Info </PageSubtitle>
+
+      <PageTitle> Unit Vendor </PageTitle>
+    </template>
+
+    <template #content>
+      <HQUnitVendorContent />
+    </template>
+  </BasePageLayout>
+</template>
+
+<script>
+import { HQ_UNITS_TABS } from '~/constants/constants'
+import { unitMaintenanceMixin } from '~/mixins/unitMaintenanceMixin'
+import { sideBarNavTabsMixin } from '~/mixins/sideBarNavTabsMixin'
+import BasePageLayout from '~/components/BasePageLayout.vue'
+import SideBar from '~/components/SideBar.vue'
+import PageSubtitle from '~/components/PageSubtitle.vue'
+import PageTitle from '~/components/PageTitle.vue'
+import HQUnitVendorContent from '~/components/HQUnitVendorContent'
+export default {
+  name: 'UnitVendorPage',
+  components: {
+    BasePageLayout,
+    SideBar,
+    PageSubtitle,
+    PageTitle,
+    HQUnitVendorContent,
+  },
+  mixins: [unitMaintenanceMixin, sideBarNavTabsMixin(HQ_UNITS_TABS)],
+  layout: 'hqlayout',
+}
+</script>
+
+<style lang="scss" scoped></style>
