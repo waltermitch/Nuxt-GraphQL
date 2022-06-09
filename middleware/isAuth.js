543006e73d35097aa77d8be894d97f6449152175
waltermitch <devmitch256@gmail.com>
2022-06-09 15:27:20 +0300

    fix bug with apollo-token when user account is delete, but they still logged in

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,5 +1,22 @@
-export default function ({ app, redirect }) {
+import Me from '../graphql/queries/me.query.gql'
+
+export default async function ({ app, redirect }) {
   const hasToken = !!app.$apolloHelpers.getToken()
+
+  try {
+    await app.apolloProvider.defaultClient.query({
+      query: Me,
+      fetchPolicy: 'no-cache',
+    })
+  } catch (error) {
+    const errorObj = error.graphQLErrors[0]
+
+    if (errorObj.message === 'Unauthenticated.') {
+      app.$apolloHelpers.onLogout()
+      return redirect('/login')
+    }
+  }
+
   if (!hasToken) {
     return redirect('/login')
   }
